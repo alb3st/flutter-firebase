38286bf479389ca40b47785ec66686b2f9be563c
Aldian Umbu <aldian@bhaktisemesta.ac.id>
2025-03-03 08:51:10 +0700

    tambahan notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pertemuan_firebase/api/firebase_api.dart';
 // import 'package:pertemuan_firebase/login_page.dart';
 import 'package:pertemuan_firebase/auth/main_page.dart';
+import 'package:pertemuan_firebase/firebase_options.dart';
+import 'package:pertemuan_firebase/pages/notification_page.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final firebaseApi = FirebaseApi();
+  await firebaseApi.initializeNotification();
   runApp(const MyApp());
 }
 
@@ -22,6 +29,10 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MainPage(),
+      navigatorKey: navigatorKey,
+      routes: {
+        '/notification_screen': (context) => const NotificationPage(),
+      },
     );
   }
 }
